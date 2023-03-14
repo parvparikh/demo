a61f6ec9183d3db73d19a556cf852da26e1a8996
parv <parvparikh11@gmail.com>
2023-03-14 15:12:54 +0530

    added sub.java

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String[] args) {          
+        int a = 65;  
+          int b = 35;  
+           
+          System.out.println("The subtraction of a and b is = " + (a-b));  
+      }  
+}
