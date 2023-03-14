3aba746dda1af015328a36628e7d87cf78f2b6a6
parv <parvparikh11@gmail.com>
2023-03-14 14:57:48 +0530

    adding mul.java

---
+++ mul.java
@@ -0,0 +1,8 @@
+public class mul {
+    public static void main(String[] args) {          
+        int a = 65;  
+          int b = 35;  
+           
+          System.out.println("The product of a and b is = " + (a*b));  
+      }  
+}
